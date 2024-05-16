7a04f8549e343adecc96559293612f9bc42ad295
Estefania <93783566+estefaniawijaya@users.noreply.github.com>
2024-05-16 08:58:47 +0700

    Initial Commit

---
+++ Simplician/SimplicianApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplicianApp.swift
+//  Simplician
+//
+//  Created by Fani on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SimplicianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
